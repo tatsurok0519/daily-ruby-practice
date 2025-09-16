0af269b21a2dfc7ec37b3d8ac7165222eeb01dac
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-09-16 09:48:39 +0900

    20

---
+++ 20.rb
@@ -0,0 +1,13 @@
+def sleep_in(is_weekday, is_vacation)
+  if is_weekday && is_vacation == false
+    puts "False"
+  else
+    puts "True"
+  end
+end
+
+# 呼び出し例
+sleep_in(false, false)
+sleep_in(true, false)
+sleep_in(false, true)
+sleep_in(true, true)
